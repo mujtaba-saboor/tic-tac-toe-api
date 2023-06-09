8ee32bdbb96c7457703e4809a03eed17757b91b4
mujtaba-saboor <mujtabasaboor27@gmail.com>
2023-06-10 00:03:18 +0500

    cookie logic handled

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,8 +19,13 @@ class ApplicationController < ActionController::API
   end
 
   def set_csrf_cookie
+    if cookies['CSRF-TOKEN'].present? && JSON.parse(response.body)['game_board_auth_token'].present? && (cookies['CSRF-TOKEN'] != JSON.parse(response.body)['game_board_auth_token'])
+      cookie_value = JSON.parse(response.body)['game_board_auth_token']
+    else
+      cookie_value = cookies['CSRF-TOKEN'].presence || JSON.parse(response.body)['game_board_auth_token']
+    end
     cookies['CSRF-TOKEN'] = {
-      value: cookies['CSRF-TOKEN'].presence || JSON.parse(response.body)['game_board_auth_token'],
+      value: cookie_value,
       domain: :all
     }
   end
